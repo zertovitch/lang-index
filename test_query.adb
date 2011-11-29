2e2c5bb1a707b4d95582a0dcda4c10b5262cd1c5
gdemont <gdemont@665d471c-e05a-4129-b6bb-97cb494e72ca>
2011-11-29 22:54:22 +0000

    Test query with test site

--- test_query.adb
+++ test_query.adb
@@ -4,8 +4,10 @@ with Ada.Text_IO;                       use Ada.Text_IO;
 with Ada.Strings.Unbounded;             use Ada.Strings.Unbounded;
 
 procedure Test_query is
-  qry: constant String:= "http://www.bing.com/search?q=%2B%22Les%20violons%20longs%22&cc=gb";
-  --- qry: constant String:= "http://www.google.com/search?q=%2B%22Les%20violons%20longs%22";
+  qry: constant String:= "http://www.spyber.com/";
+  -- ^ that site gets client info, like OS, navigator,...
+  -- qry: constant String:= "http://www.bing.com/search?q=%2B%22Les%20violons%20longs%22&cc=gb";
+  -- qry: constant String:= "http://www.google.com/search?q=%2B%22Les%20violons%20longs%22";
   web: constant String:= AWS.Response.Message_Body(AWS.Client.Get(qry));
   f: File_Type;
 begin
